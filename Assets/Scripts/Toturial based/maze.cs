1da7b15c05266d49a35e1cc17e93225f7211c9c4
Joshua Stevenson <JoshuaStevenson97@gmail.com>
2016-11-22 14:51:41 +0000

    Fixed all the errors :D
    Fixed code with some short explainations are in comments, feel free to
    ask me about them :3

--- Assets/Scripts/Toturial based/maze.cs
+++ Assets/Scripts/Toturial based/maze.cs
@@ -1,25 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class maze : MonoBehaviour {
+/* Notes on the code:
+ * You kinda screwed up one of the brackets and that was causing a lot of the issues ^^
+ * 
+ */
 
-	public int sizeX, sizeZ;
-	public mazeCell cellPrefab;
+public class maze : MonoBehaviour
+{
 
-	private mazeCell[,] cells;
+    public int sizeX, sizeZ;
+    public mazeCell cellPrefab;
 
-	public void Generate () {
-		cells = new mazeCell[sizeX; sizeZ];
-		for (int x = 0; x < sizeX; x++) {
-			createCell (x, sizeZ);
-		}
-	}
-}
-private void createCell (int x, int z) {
-	mazeCell newCell = Instantiate (cellPrefab) async mazeCell;
-	cells[x, z] = newCell;
-	newCell.name = "Mace Cell " + x + ", " + z;
-	newCell.transform.parent = transform;
-	newCell.transform-localPosition = new Vector3 (x - sizeX * 0.5f, 0f, z - sizeZ * 0.5f + 0.5f);
+    private mazeCell[,] cells;
 
+    public void Generate()
+    {
+        cells = new mazeCell[sizeX, sizeZ]; //cells = new mazeCell[sizeX; sizeZ]; (You used a semi-colon on the array declaration)
+        /*for (int x = 0; x < sizeX; x++)
+        {
+            createCell(x, sizeZ); (Was only going through the X dimention of the array, also sizeZ is technically outside of the array, ask me about that later)
+        }
+        */ 
+        for (int x = 0; x < sizeX; x++)
+        {
+            for (int z = 0; z < sizeZ; z++)
+            {
+                createCell(x, z);
+            }
+        }
+    }
+    private void createCell(int x, int z)
+    {
+        mazeCell newCell = Instantiate(cellPrefab) as mazeCell; //mazeCell newCell = Instantiate (cellPrefab) async mazeCell; (You used 'async' instead of 'as', blame autocorrect ^^ )
+        cells[x, z] = newCell;
+        newCell.name = "Mace Cell " + x + ", " + z;
+        newCell.transform.parent = transform;
+        newCell.transform.localPosition = new Vector3(x - sizeX * 0.5f, 0f, z - sizeZ * 0.5f + 0.5f);
+
+    }
 }
